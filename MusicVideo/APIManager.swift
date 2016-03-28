3624a8a760fd7a1c6818927cd65d62d92844fa08
Kalpesh <kalpesh@girishs-mac-mini.local>
2016-03-28 11:16:59 +0530

    Completed Part 3.

--- MusicVideo/APIManager.swift
+++ MusicVideo/APIManager.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Kalpesh on 3/2/16.
 //  Copyright © 2016 Bigrattle Technologies. All rights reserved.
-//
 
 import Foundation
 
@@ -12,21 +11,32 @@ class APIManager{
     func loadData(urlString:String, completion: (result:String) -> Void){
         
         let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
-        
         let session = NSURLSession(configuration: config)
-        
-//        let session  = NSURLSession.sharedSession()WS
+//        let session  = NSURLSession.sharedSession()
         let url = NSURL(string: urlString)!
-        
         let task = session.dataTaskWithURL(url){
-            (data,response, error) -> Void in
-            dispatch_async(dispatch_get_main_queue()){
-                if error != nil {
-                    completion(result: (error?.localizedDescription)!)
+            (data, response, error) -> Void in
+            if error != nil{
+                dispatch_async(dispatch_get_main_queue()){
+                    completion(result: (error!.localizedDescription))
                 }
-                else{
-                    completion(result: "NSURLSession Successuful")
-                    print(data)
+            }
+            else{
+                do{
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject]{
+                    print(json)
+                    let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                        dispatch_async(dispatch_get_global_queue(priority, 0)){
+                    dispatch_async(dispatch_get_main_queue()){
+                        completion(result: "JSON Serialization successs")
+                    }
+                }
+                }
+                }
+                catch{
+                    dispatch_async(dispatch_get_main_queue()){
+                       completion(result: "error in NSJSONserialization")
+                    }
                 }
             }
         }
