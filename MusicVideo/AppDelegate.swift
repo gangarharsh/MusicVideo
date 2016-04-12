8ce47c736bb97db6d3261119161f5d30949b9fc9
Tejas <tejas@tejass-mac-mini.local>
2016-04-12 16:07:19 +0530

    part-10 completed

--- MusicVideo/AppDelegate.swift
+++ MusicVideo/AppDelegate.swift
@@ -26,6 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         internetCheck = Reachability.reachabilityForInternetConnection()
         internetCheck?.startNotifier()
+        statusChangedWithReachability(internetCheck!)
         return true
     }
     
